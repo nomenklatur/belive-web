e3bb83cb9eba5a2a0b8c19786f3306263c5d51e8
Dimas <dimasekaputra2000@gmail.com>
2025-06-22 22:23:19 +0700

    feat: remove laravel brandings, fonts, and icons

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,4 +1,3 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
@@ -15,14 +14,15 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
+                            {/* <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
                                 <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
                             </div>
-                            <span className="sr-only">{title}</span>
+                            <span className="sr-only">{title}</span> */}
+                            <img src="/logo.svg" alt="" width={70} />
                         </Link>
 
                         <div className="space-y-2 text-center">
-                            <h1 className="text-xl font-medium">{title}</h1>
+                            <h1 className="text-xl font-medium font-manrope">{title}</h1>
                             <p className="text-center text-sm text-muted-foreground">{description}</p>
                         </div>
                     </div>
