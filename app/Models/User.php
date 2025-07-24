0b15742870930421c0a3a29173a0c5a1492d70a1
Dimas Eka Putra <dimasekaputra2000@gmail.com>
2025-07-24 09:35:57 +0700

    Revert "feat: create role model and migration"

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,16 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role_id',
     ];
-
-    /**
-     * Get the role associated with the user.
-     */
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
 
     /**
      * The attributes that should be hidden for serialization.
